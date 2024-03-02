08aa21e7ffc9836cd157ad85ce5263f7fc834146
gmaringa <156018947+gmaringa@users.noreply.github.com>
2024-03-02 23:59:24 +0300

    controller update

--- app/api/controllers/user.controller.js
+++ app/api/controllers/user.controller.js
@@ -5,20 +5,22 @@ export const createOrUpdateUser = async(id, first_name, last_name, profile_image
     try{
         await connectToDB();
     
-        const user = await User.findOneAndUpdate({clerkId: id}, {
-            $set: {
-                clerkId: id,
+        const user = await User.findOneAndUpdate(
+            { clerkId: id },
+            {
+              $set: {
                 firstName: first_name,
                 lastName: last_name,
                 profilePhoto: profile_image_url,
                 email: email_addresses[0].email_address,
-            }
-        }, 
-        {upsert: true, new: true})
-
-        await user.save();
-        return user;
-    } catch(error) {
-        console.log(error);
-    }
+              },
+            },
+            { upsert: true, new: true } // if user doesn't exist, create a new one
+          );
+      
+          await user.save();
+          return user;
+        } catch (error) {
+          console.error(error);
+        }
 }
