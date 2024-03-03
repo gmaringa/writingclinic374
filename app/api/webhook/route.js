de12da7c2731e2a23643e3510a30dcb803b349ef
gmaringa <156018947+gmaringa@users.noreply.github.com>
2024-03-03 15:04:40 +0300

    route update

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,79 +1,102 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { createOrUpdateUser } from "../../../lib/controllers/user.controller.js";
+// import { createOrUpdateUser } from "../../../lib/controllers/user.controller.js";
+
+import { db } from "../../../lib/models/Users.model.js"
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
+    )
   }
 
   // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
-
-  console.log(svix_id, svix_timestamp, svix_signature)
+  const headerPayload = headers()
+  const svix_id = headerPayload.get("svix-id")
+  const svix_timestamp = headerPayload.get("svix-timestamp")
+  const svix_signature = headerPayload.get("svix-signature")
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
+      status: 400
+    })
   }
 
   // Get the body
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  // Create a new SVIX instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET)
 
-  let evt;
+  let evt
 
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    });
+      "svix-signature": svix_signature
+    })
   } catch (err) {
-    console.error("Error verifying webhook:", err);
+    console.error("Error verifying webhook:", err)
     return new Response("Error occured", {
-      status: 400,
-    });
+      status: 400
+    })
   }
 
-  // Handle the event
-  const eventType = evt?.type;
-
-  if (eventType === "user.created" || eventType === "user.updated") {
-    const { id, first_name, last_name, profile_image_url, email_addresses } =
-      evt?.data;
-
+  if (evt) {
+    // 👉 Parse the incoming event body into a ClerkWebhook object
     try {
-      await createOrUpdateUser(
-        id,
-        first_name,
-        last_name,
-        profile_image_url,
-        email_addresses,
-      );
+      // 👉 `webhook.type` is a string value that describes what kind of event we need to handle
 
-      return new Response("User is created or updated", {
-        status: 200,
-      });
+      // 👉 If the type is "user.updated" the important values in the database will be updated in the users table
+      if (evt.type === "user.updated") {
+        await db.user.update({
+          where: { externalId: evt.data.id },
+          data: {
+            username: evt.data.username || "",
+            displayName: `${evt.data.first_name} ${evt.data.last_name}`,
+            imageUrl: evt.data.image_url,
+            email: evt.data.email_addresses[0]?.email_address || ""
+          }
+        })
+      }
+
+      // 👉 If the type is "user.created" create a record in the users table
+      if (evt.type === "user.created") {
+        const emailAddress = evt.data.email_addresses[0]?.email_address
+        await db.user.create({
+          data: {
+            externalId: evt.data.id,
+            username:
+              evt.data.username ??
+              `${emailAddress.split("@")[0]}-${evt.data.id}`,
+            displayName: `${evt.data.first_name} ${evt.data.last_name}`,
+            imageUrl: evt.data.image_url,
+            email: emailAddress
+          }
+        })
+      }
+
+      // 👉 If the type is "user.deleted", delete the user record and associated blocks
+      if (evt.type === "user.deleted") {
+        await db.user.delete({
+          where: { externalId: evt.data.id }
+        })
+      }
+
+      return new Response("", { status: 201 })
     } catch (err) {
-      console.error("Error creating or updating user:", err);
-      return new Response("Error occured", {
-        status: 500,
-      });
+      console.error(err)
+      return new Response("Error occured -- processing webhook data", {
+        status: 500
+      })
     }
   }
 }
